81c03d9b02ab1a10b42ac28fff49bff780a71e1f
record_I <73824204+imlzr@users.noreply.github.com>
2021-07-22 13:05:44 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -16,7 +16,6 @@ def fun(filename):
     for li in li_list:
         content = li.xpath('./td[2]/a/text()')[0]
         detail_url ='http:/'+ li.xpath('./td[2]/a/@href')[0]
-        detail_url = detail_url.split('&')[0]
         list = detail_url.split('%')
         detail_url = list[0]+'%'+list[1]+str(content)+'%'+list[-1]
         fp.write(f'[{content}]({detail_url})')
