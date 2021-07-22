569b298e576136f7e7bcb4a53ef8fd79d0bf06b5
record_I <73824204+imlzr@users.noreply.github.com>
2021-07-22 08:42:49 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -32,6 +32,6 @@ if __name__ == '__main__':
     if not os.path.exists():
         os.mkdir(path)
     
-    filename = path +'/'+nowhour+':'+nowminites+'.md'
+    filename = path +'/'+nowhour+'：'+nowminites+'.md'
 
     fun(filename)
